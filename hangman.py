b792b2572930fe7b335e4bfb88359664b5050f3e
Aditya Shah <shahaditya@outlook.com>
2021-12-17 13:53:40 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,19 +1,45 @@
 import random
+LIVES = 6
 
-words = ["monkey", "banana", "scale", "team", "rabid", "scary", "charge", "lamp", "man", "lip", "scribble", "acid", "experience"]
-chosen_word = (random.choice(words))
-print(chosen_word)
-string_number =  len(chosen_word)
-print("There are", string_number, "letters in this word")
-number_guesses = int(input("How many times would you like to guess?"))
-guessed_letter = input("Guess a letter?")
-i = 1
-for i in number_guesses:
-    if guessed_letter in chosen_word:
-        print("you guessed a letter")
-    else:
-        print("That letter is not in the random word")
-    i = i+1
+def ChooseWord():
+  words = {"Apple":"Fruit", "Truck":"Vehicle", "Purple":"Color"}
+  word = random.choice(list(words.keys()))
+  return word.lower(), words[word]
 
-#print "you get 10 tries to geuss the word"
-#random word from list. Ask to guess letter
+def CreateWordGuessed(word):
+  wordList = []
+  for i in range(len(word)):
+    wordList.append('-')
+  return wordList
+
+def GetLetterFromUser():
+  letter = input("Pick Letter: ").lower()
+  while len(letter) != 1:
+    print("Invalid input, please try again...")
+    letter = input("Pick Letter: ").lower()
+  return letter
+
+def FitLetterInWordGuessed(word, letter, wordGuessed):
+  match = False
+  for i in range(len(word)):
+    if word[i] == letter:
+      wordGuessed[i] = letter
+      match = True
+  return wordGuessed
+
+def main():
+  word, wordHint = ChooseWord()
+  print(word)
+  wordGuessed = CreateWordGuessed(word)
+  counter = 0
+  while True:
+    print("".join(wordGuessed)+" (hint:"+wordHint+")")
+    letter = GetLetterFromUser()
+    wordGuessed, match = FitLetterInWordGuessed(word, letter, wordGuessed)
+    if match == False:
+      counter += 1
+      if counter == LIVES:
+        print("Game Over!")
+        break
+
+main()
