bb8a26a920454aa5898a8bc65a76da828a0c38fd
cwkadi25355 <shahaditya@outlook.com>
2021-12-26 17:45:47 -0800

    Quadratic Problem Solver

---
+++ quadratic formula solver.py
@@ -0,0 +1,37 @@
+import math
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+denominator = 2 * a
+determinant = b**2 - 4*a*c
+total_sqrt = math.sqrt(determinant)
+
+#vertex
+print("X of Vertex is \n" + str(-(b)/(2*a)))
+
+# x intercepts
+try: 
+    x1 = (-(b)+ total_sqrt)/denominator
+    print("x1: ")
+    print(x1)
+
+    try:
+        x2 = (-(b)- total_sqrt)/denominator
+        print("x2: ")
+        print(x2)
+    except:
+        print("no - answer")
+
+
+except:
+
+    try: 
+        print("no + answer")
+        x2 = (-(b)- total_sqrt)/denominator
+        print(x2)
+    except:
+        print("No soloutions")
+
+
